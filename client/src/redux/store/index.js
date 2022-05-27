3072840501391a314ee1979c2017d98d5bda855f
malhee <emilianodicampli@hotmail.com>
2022-05-27 15:30:29 -0300

    lestgo

---
+++ client/src/redux/store/index.js
@@ -0,0 +1,10 @@
+import {createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";  // DUDA !!!!!!!!!!!!
+import thunk from "redux-thunk;"
+import rootReducer from "../reducer";
+
+
+
+
+
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))  // DUDA !!!!!
